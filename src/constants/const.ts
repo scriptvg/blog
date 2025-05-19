290b9fbc666a0ad58dcd8709ac48b230bd4ec27c
scriptvg <velezalan34@gmail.com>
2025-05-18 23:27:23 -0600

    first commit

---
+++ src/constants/const.ts
@@ -0,0 +1,5 @@
+export const TITLE_PAGE = 'My Blog';
+
+export const TITLE = 'My Blog';
+export const DESCRIPTION = 'My Blog Description';
+
---
+++ src/constants/nav.ts
@@ -0,0 +1,10 @@
+export const TITLE_NAV = "Kromm's Blog";
+
+export const NAV_LINKS = [
+  { name: "Home", href: "#home" },
+  { name: "Manga", href: "/manga" },
+  { name: "Series", href: "#series" },
+  { name: "Películas", href: "#peliculas" },
+  { name: "Contacto", href: "#contacto" },
+  { name: "Portafolio", href: "#portfolio" },
+];
