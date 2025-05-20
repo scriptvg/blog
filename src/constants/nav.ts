c5537f40b8a677045f94c62ba7f54f018d61bced
scriptvg <49818250+scriptvg@users.noreply.github.com>
2025-05-19 18:42:52 -0600

    changes

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -8,3 +8,15 @@ export const NAV_LINKS = [
   { name: "Contacto", href: "/contacto" },
   { name: "Portafolio", href: "/portfolio" },
 ];
+
+
+export const TITLE_NAV_MANGA = "Kromm's Manga";
+
+export const NAV_LINKS_MANGA = [
+  { name: "Inicio", href: "/manga" },
+  { name: "Manga", href: "/manga" },
+  { name: "Series", href: "/series" },
+  { name: "Películas", href: "/peliculas" },
+  { name: "Contacto", href: "/contacto" },
+  { name: "Portafolio", href: "/portfolio" },
+];
